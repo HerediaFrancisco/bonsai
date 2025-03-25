9f9c93ac87b76692c2cc21c74f22f1d5d163a9b6
Francisco Heredia <flheredia22@gmail.com>
2025-03-25 20:45:31 -0300

    primer commit

---
+++ apiBonsai/src/test/java/com/bonsai/apiBonsai/ApiBonsaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonsai.apiBonsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBonsaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
